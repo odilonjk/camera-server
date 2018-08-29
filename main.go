f3464f05dbe25b1f14127aefcc4e834c4be6ec90
OdilonJK <kroger.odilonj@gmail.com>
2018-08-28 22:42:29 -0300

    Added the basic usage of GoCV lib to save a video

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
+	"gocv.io/x/gocv"
 )
+
+var startCamera = 0
 
 func main() {
 	router := mux.NewRouter()
@@ -20,6 +26,39 @@ func startRecord(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(405), 405)
 		return
 	}
+	webcam, err := gocv.OpenVideoCapture(0)
+	if err != nil {
+		fmt.Printf("Error on opening video capture.")
+		return
+	}
+	defer webcam.Close()
+	img := gocv.NewMat()
+	defer img.Close()
+
+	if ok := webcam.Read(&img); !ok {
+		fmt.Printf("Cannot read camera device.")
+		return
+	}
+
+	fileName := strconv.FormatInt(time.Now().Unix(), 10) + ".avi"
+	writer, err := gocv.VideoWriterFile(fileName, "MJPG", 25, img.Cols(), img.Rows(), true)
+	if err != nil {
+		fmt.Printf("Error opening video writer device")
+		return
+	}
+	defer writer.Close()
+	startCamera = 1
+	for startCamera > 0 {
+		if ok := webcam.Read(&img); !ok {
+			fmt.Printf("Device closed.")
+			return
+		}
+		if img.Empty() {
+			continue
+		}
+
+		writer.Write(img)
+	}
 }
 
 func stopRecord(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +66,7 @@ func stopRecord(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(405), 405)
 		return
 	}
+	startCamera = 0
 }
 
 func getRecord(w http.ResponseWriter, r *http.Request) {
